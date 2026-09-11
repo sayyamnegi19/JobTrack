195a96f68072297ecf3c16032b30c3c1c944e287
Sayyam Singh <sayyamnegi19@gmail.com>
2026-09-11 19:08:41 +0530

    Fix: Custom user model authentication

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,44 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The email field must be set.")
+
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if not extra_fields.get("is_staff"):
+            raise ValueError("SuperUser must have is_staff=True.")
+        if not extra_fields.get("is_superuser"):
+            raise ValueError("SuperUser must have is_super=True.")
+        
+        return self.create_user(
+            email=email,
+            password=password,
+            **extra_fields
+        )
+    
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = "email"
-
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
