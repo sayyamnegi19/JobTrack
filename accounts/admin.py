3b187869a500c89180b4a4e589410a59989023ed
Sayyam Singh <sayyamnegi19@gmail.com>
2026-09-11 20:22:13 +0530

    Feat: configure custom user admin for custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
 
 # Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    ordering = ('email',)
+
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff"
+    )
+
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name"
+    )
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "password"
+                )
+            }
+        ),
+        (
+            "Personel Info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name"
+                )
+            }
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined"
+                )
+            }
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser"
+                )
+            }
+        ),
+    )
